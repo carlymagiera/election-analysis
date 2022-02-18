b9ec4d9d936889a1cce43253286cb9d4768fa04d
Carly Magiera <carlymagiera@Carlys-MacBook-Pro.local>
2022-02-18 13:12:29 -0800

    adding PyPoll.py file

---
+++ python_practice.py
@@ -0,0 +1,61 @@
+print("hello world")
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+    print("turn on AC.")
+else:
+    print("Open the window.")
+
+#what is the score?
+score=int(input("What is your test score?"))
+
+#determine grade.
+if score>=90:
+    print('your grade is an A')
+else:
+    if score>= 80:
+        print('your grade is a B')
+    else:
+        if score >=70:
+            print('your grade is a C.')
+        else:
+            if score >= 60:
+                print ('Your grade is a D.')
+            else:50
+            print('your grade is an F.')
+
+
+    if "El Paso" in counties:
+        print("El Paso is in list of counties.")
+    else: 
+        print("El Paso is not in the list of countries")
+for county in counties
+    print(county)
+
+for num in range(5):
+    print(num)
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in count_dict.values():
+        print(value)
+
+
+import csv
+dir(csv)
+
+dir(int)
+
+import random
+dir(random)
+
